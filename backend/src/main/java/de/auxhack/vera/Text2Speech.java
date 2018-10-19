2212dd23d3e3e680439f547861df4a94125a4915
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-10-19 16:32:47 +0200

    feat: Add Samplecodes for Google Cloud Speek

---
+++ backend/src/main/java/de/auxhack/vera/Text2Speech.java
@@ -0,0 +1,73 @@
+package de.auxhack.vera;
+
+import com.google.cloud.texttospeech.v1.AudioConfig;
+import com.google.cloud.texttospeech.v1.AudioEncoding;
+import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
+import com.google.cloud.texttospeech.v1.SynthesisInput;
+import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
+import com.google.cloud.texttospeech.v1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
+import com.google.protobuf.ByteString;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+/**
+ * backend
+ * Author: tsteidle
+ * Created: 19.10.18
+ */
+public class Text2Speech {
+
+    public static void main(String... args) throws Exception {
+        final JFXPanel fxPanel = new JFXPanel();
+
+        // Instantiates a client
+        try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
+            // Set the text input to be synthesized
+            SynthesisInput input = SynthesisInput.newBuilder()
+                    .setText("Danke.")
+                    .build();
+
+            // Build the voice request, select the language code ("en-US") and the ssml voice gender
+            // ("neutral")
+            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                    .setLanguageCode("de-DE")
+                    .setSsmlGender(SsmlVoiceGender.FEMALE)
+                    .setName("de-DE-Wavenet-A")
+                    .build();
+
+            // Select the type of audio file you want returned
+            AudioConfig audioConfig = AudioConfig.newBuilder()
+                    .setAudioEncoding(AudioEncoding.MP3)
+                    .build();
+
+            // Perform the text-to-speech request on the text input with the selected voice parameters and
+            // audio file type
+            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice,
+                    audioConfig);
+
+            // Get the audio contents from the response
+            ByteString audioContents = response.getAudioContent();
+
+            // Write the response to the output file.
+            try (OutputStream out = new FileOutputStream("output.mp3")) {
+                out.write(audioContents.toByteArray());
+                System.out.println("Audio content written to file \"output.mp3\"");
+
+                String bip = "output.mp3";
+                Media hit = new Media(new File(bip).toURI().toString());
+                MediaPlayer mediaPlayer = new MediaPlayer(hit);
+                mediaPlayer.play();
+            }
+
+
+        }
+    }
+}
